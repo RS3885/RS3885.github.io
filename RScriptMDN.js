f7910e74be4d520d2f5b436e587dbde3deab8a33
RS3885 <96733377+RS3885@users.noreply.github.com>
2021-12-28 14:44:02 +0000

    Add files via upload

---
+++ RScriptMDN.js
@@ -0,0 +1,18 @@
+function runMe(stR) {
+    alert('MDN rs is here again in a src file. Im learning fast!');
+    console.log(stR);
+    alert(stR);
+}
+
+runMe("fred MDN");
+
+
+const HelloVueApp = {
+    data() {
+      return {
+        message: 'Hello VueyxxxRwwwwwwwwwwwwwww!!'
+      }
+    }
+  }
+  
+  Vue.createApp(HelloVueApp).mount('#hello-vue')
