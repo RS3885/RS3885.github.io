e18893deae85b4f69f2962f035ca1f552dc1ec05
RS3885 <rspendiff@gmail.com>
2022-01-10 14:54:23 +0000

    u9

--- RScriptMDN.js
+++ RScriptMDN.js
@@ -4,16 +4,28 @@ function runMe(stR) {
     alert(stR);
 }
 
-runMe("Get that job");
+runMe("Good luck with the job applications,\nMim");
 
-
+/*
 const HelloVueApp = {
     data() {
       return {
         message: 'How is the job hunting going? There will be more coming to this page so be sure to revisit occassionaly.\
-        \n Very basic looking but there is a lot more going on under the hood.'
+        \nVery basic looking but there is a lot more going on under the hood.'
       } 
     }
   }
   
   Vue.createApp(HelloVueApp).mount('#hello-vue')
+  */
+
+
+function mimStr() {
+  let mStr = document.getElementById("mim");
+  mStr.innerText = 'How is the job hunting going? There will be more coming to this page so be sure to revisit occassionaly.\
+  \nVery basic looking but there is a lot more going on under the hood.';
+}
+
+mimStr();
+
+
